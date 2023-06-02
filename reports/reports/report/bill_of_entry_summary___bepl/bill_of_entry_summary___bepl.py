6d45326c08b139ffd32efc14e290b84131257ab0
KishanPanchal25 <k.d.panchalofc@gmail.com>
2023-06-02 12:51:24 +0530

    Landed cost voucher

--- reports/reports/report/bill_of_entry_summary___bepl/bill_of_entry_summary___bepl.py
+++ reports/reports/report/bill_of_entry_summary___bepl/bill_of_entry_summary___bepl.py
@@ -3,7 +3,11 @@
 
 import frappe
 from frappe import _
+#  import logger
+from frappe.utils import logger
 
+logger.set_log_level("DEBUG")
+logger = frappe.logger("bill_of_entry", allow_site=True, file_count=1)
 
 def execute(filters=None):
     validate_filters(filters)
@@ -39,6 +43,12 @@ def validate_filters(filters=None):
 
 def get_data(filters):
     bill_of_entry = frappe.qb.DocType("Bill of Entry")
+    journal_entry_account = frappe.qb.DocType("Journal Entry Account")
+    purchase_invoice = frappe.qb.DocType("Purchase Invoice") 
+    landed_cost_voucher = frappe.qb.DocType("Landed Cost Voucher")
+    landed_cost_purchase_receipt = frappe.qb.DocType("Landed Cost Purchase Receipt")
+    landed_cost_taxes_and_charges = frappe.qb.DocType("Landed Cost Taxes and Charges")
+    purchase_invoice_item = frappe.qb.DocType("Purchase Invoice Item")
 
     query = (
         frappe.qb.from_(bill_of_entry)
@@ -54,6 +64,16 @@ def get_data(filters):
             bill_of_entry.total_customs_duty,
             bill_of_entry.total_taxes,
             bill_of_entry.total_amount_payable,
+            journal_entry_account.parent.as_("payment_journal_entry"),
+            purchase_invoice.supplier,
+            purchase_invoice.supplier_name,
+            purchase_invoice.posting_date,
+            purchase_invoice.bill_no,
+            purchase_invoice.bill_date,
+            purchase_invoice.inward_gate_no,
+            purchase_invoice.inward_gate_entry_date,
+            landed_cost_taxes_and_charges.expense_account,
+            landed_cost_taxes_and_charges.amount 
         )
         .where(bill_of_entry.docstatus == 1)
         .where(
@@ -62,41 +82,23 @@ def get_data(filters):
             ]
         )
         .where(bill_of_entry.company == filters.get("company"))
-    )
-
-    query = update_journal_entry_for_payment(query)
-    query = update_purchase_invoice_query(query)
-    return query.run(as_dict=1)
-
-
-def update_journal_entry_for_payment(query):
-    bill_of_entry = frappe.qb.DocType("Bill of Entry")
-    journal_entry_account = frappe.qb.DocType("Journal Entry Account")
-
-    return (
-        query.left_join(journal_entry_account)
+        .left_join(journal_entry_account)
         .on(bill_of_entry.name == journal_entry_account.reference_name)
-        .select(journal_entry_account.parent.as_("payment_journal_entry"))
-    )
-
-
-def update_purchase_invoice_query(query):
-    bill_of_entry = frappe.qb.DocType("Bill of Entry")
-    purchase_invoice = frappe.qb.DocType("Purchase Invoice")
-
-    return (
-        query.left_join(purchase_invoice)
+        .left_join(purchase_invoice)
         .on(purchase_invoice.name == bill_of_entry.purchase_invoice)
-        .select(
-            purchase_invoice.supplier,
-            purchase_invoice.supplier_name,
-            purchase_invoice.posting_date,
-            purchase_invoice.bill_no,
-            purchase_invoice.bill_date,
-            purchase_invoice.inward_gate_no,
-            purchase_invoice.inward_gate_entry_date,
-        )
+        .left_join(purchase_invoice_item)
+        .on(purchase_invoice_item.parent == purchase_invoice.name)
+        .left_join(landed_cost_purchase_receipt)
+        .on(landed_cost_purchase_receipt.receipt_document == purchase_invoice_item.purchase_receipt)
+        .left_join(landed_cost_voucher)
+        .on(landed_cost_voucher.name == landed_cost_purchase_receipt.parent)
+        .left_join(landed_cost_taxes_and_charges)
+        .on(landed_cost_taxes_and_charges.parent == landed_cost_voucher.name)
+        
     )
+
+
+    return query.run(as_dict=1)
 
 
 def get_columns():
@@ -207,5 +209,18 @@ def get_columns():
             "label": _("Amount Payable"),
             "fieldtype": "Currency",
             "width": 90,
+        },
+        {
+            "fieldname": "expense_account",
+            "label": _("Expense Account"),
+            "fieldtype": "Link",
+            "options": "Account",
+            "width": 230,
+        },
+        {
+            "fieldname": "amount",
+            "label": _("Amount"),
+            "fieldtype": "Currency",
+            "width": 100,
         },
     ]
